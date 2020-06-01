cbf399181e2828fbe21a30d31d8e65092c68d9e0
Mattee37 <bluestacksmm.137@gmail.com>
2020-06-01 04:54:28 -0300

    adding 'test' page

---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+import AboutUsContent from "../components/AboutUsContent";
+
+const test = () => (
+  <Layout>
+    <AboutUsContent />
+  </Layout>
+);
+
+export default test;
